74f1e563285ab116c271f346cf61eb1a2bd71570
Sinovuyo97 <sinovuyoshakes@gmail.com>
2021-06-25 10:33:49 +0200

    services

---
+++ src/components/services.js
@@ -0,0 +1,56 @@
+function Services() {
+  return (
+    <div className="container">
+      <div className="box">
+        <div className="icon"><p><i class="fas fa-hand-holding-usd"></i></p></div>
+        <div className="content">
+          <h3>FUNDRAISING</h3>
+          <p>
+            {" "}
+            Fundraising is a significant way
+                <br /> that non-profit organizations may
+                <br />
+                obtain the money for their operations.
+                <br /> These operations can involve a very broad <br />
+                array of concerns such as religious or...
+          </p>
+          <a href="#">READ MORE</a>
+        </div>
+      </div>
+
+      <div className="box">
+        <div className="icon"><i class="fas fa-hand-holding-heart"></i></div>
+        <div className="content">
+          <h3>PURPOSE</h3>
+          <p>
+            {" "}
+            They work for uplifting the backward
+            <br /> sections of the community. Moreover,
+            <br /> the major function which is performes by NGO's
+            <br /> is to bridge the gap of inequalities
+            <br /> and unfair treatments, they ardently strive...
+          </p>
+          <a href="#">READ MORE</a>
+        </div>
+      </div>
+
+      <div className="box">
+        <div className="icon"><i class="fas fa-calendar-alt"></i></div>
+        <div className="content">
+          <h3>CHARITY EVENTS</h3>
+          <p>
+            {" "}
+            Fundraising is a significant way
+            <br /> that non-profit organizations may
+            <br />
+            obtain the money for their operations.
+            <br /> These operations can involve a very broad <br />
+            array of concerns such as religious or
+          </p>
+          <a href="#">READ MORE</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Services;
